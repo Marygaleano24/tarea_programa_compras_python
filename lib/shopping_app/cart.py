1a58c002a94798552525b312bb0b5942f7e53275
MaryGaleano24 <marianagaleano846@gmail.com>
2024-07-05 11:42:32 -0400

    nuevo commit

---
+++ lib/shopping_app/cart.py
@@ -0,0 +1,36 @@
+from ownable import Ownable
+
+class Cart(Ownable):
+    from item_manager import show_items
+
+    def __init__(self, owner):
+        super().__init__()  # Llama al constructor de Ownable
+        self.set_owner(owner)  # Asigna el propietario al carrito
+        self.items = []  # Inicializa la lista de artículos en el carrito
+
+    def items_list(self):
+        return self.items
+
+    def add(self, item):
+        self.items.append(item)
+
+    def total_amount(self):
+        price_list = []
+        for item in self.items:
+            price_list.append(item.price)
+        return sum(price_list)
+
+    def check_out(self):
+        if self.owner.wallet.balance < self.total_amount():
+            print("Saldo insuficiente en la billetera del propietario del carrito.")
+            return
+        
+        for item in self.items:
+            item.owner.wallet.deposit(item.price)  # Transfiere el precio del artículo a la billetera del propietario del artículo
+            item.owner = self.owner  # Transfiere la propiedad del artículo al propietario del carrito
+        
+        self.items = []  # Vacía el carrito después de la compra
+
+        print("Compra realizada exitosamente.")
+
+    # Resto del código de la clase Cart...
